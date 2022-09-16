26d4e5e42dba18d3367d22d6d977ad123b03f6a4
agarcia50S <agarcia50@luc.edu>
2022-09-15 22:04:30 -0500

    building class that will parse and store collected html

---
+++ GroceryPrices/structure_builder.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+class Storage():
+    def __init__(self, template):
+        self.container = template
+    def find_html(self, page_html,  tag, tag_class):
+        pass
