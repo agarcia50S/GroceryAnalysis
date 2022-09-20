919d8ad9f704bae5483962e21e6b7015c59c9f32
agarcia50S <agarcia50@luc.edu>
2022-09-20 13:40:01 -0500

    formatted code into class

--- GroceryPrices/target_scrapper.py
+++ GroceryPrices/target_scrapper.py
@@ -7,57 +7,74 @@ from selenium.common.exceptions import TimeoutException
 from time import sleep
 from bs4 import BeautifulSoup
 
+class TargetScrapper():
+    def __init__(self, driver, url, last_pg_xpath, categories):
+        self.driver = driver
+        self.url = url
+        self.last_pg_xpath = last_pg_xpath
+        self.categories = categories
 
-# grocery_list = ['meat', 'produce', 'dairy', 'frozen foods', 'bread AND bakery', 
-#                 'beverages', 'baking goods', 'pantry food',  'seafood', 'snacks', 
-#                 'pastry', 'deli', 'coffee']
+    def get_page_count(self):
+        try:
+            element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, self.last_pg_xpath))
+            )
+        except TimeoutException:
+            print('Could not find element')
+        else:
+            page_num = int(element.text[-2:])
+            return page_num
 
-grocery_list = ['meat', 'produce']
+    def find_products(self):
+        for term in self.categories:
+            url_parts = self.url.split('&')
+            page_indx = 0
+            # constructing url for given item and page index
+            url = f'{url_parts[0]}{term}&{url_parts[1]}&{url_parts[2]}&{url_parts[3]}{page_indx}&{page_indx[4]}'
+            self.driver.get(url) # navigates to url; returns none
+            last_pg_num = self.get_page_count() - 1 # remove current pg from total
 
-wd = webdriver.Chrome()
+            for _ in range(last_pg_num):
+                url = f'{url_parts[0]}{term}&{url_parts[1]}&{url_parts[2]}&{url_parts[3]}{page_indx}&{page_indx[4]}'
+                wd.get(url) # navigates to url; returns none
+                sleep(4.5)
+                print(wd.page_source)
+                page_indx += 24
 
-def get_page_count(driver, xpath):
-    try:
-        element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, xpath))
-        )
-    except TimeoutException:
-        print('Could not find element')
+                # FOR TESTING
+                if page_indx >= 96: 
+                    break
+        self.driver.quit()
 
-    else:
-        page_num = int(element.text[-2:])
-        return page_num
-
-def find_products(driver, categories, url):
-    driver.get(url) # navigates to url; returns none
-    url_parts = url.split('=')
-    for i in categories:
-        page_indx = 0
-        term = i
-        # constructing url for given item and page index
-        url = url_parts[0] + term + '&' + url_parts[1] + '&' + url_parts[2] + '&' + url_parts[3] + page_indx + '&' + page_indx[4]
-
-
-
-    
-
-for i in grocery_list:
-    page_indx = 0
-    term = i
-    url = f"https://www.target.com/s?searchTerm={term}&sortBy=relevance&category=5xt1a&Nao={page_indx}&moveTo=product-list-grid"
-    wd.get(url) # navigates to url; returns none
-    sleep(5)
-    page_num_xpath = "//span[@class='Pagination__StyledSpan-sc-sq3l8r-5 gyBTAO']"
-    num_max = get_page_count(wd, page_num_xpath)
-
-    for _ in range(num_max - 1):
-        url = f"https://www.target.com/s?searchTerm={term}&sortBy=relevance&category=5xt1a&Nao={page_indx}&moveTo=product-list-grid"
-        wd.get(url) # navigates to url; returns none
-        sleep(4.5)
-        print(wd.page_source)
-        page_indx += 24
-
-wd.quit()
+    @staticmethod
+    def make_xpath(tag, attr, val):
+        '''
+        Makes a relative xpath for an html element. xpath 
+        form is //tagname[@attribute='value']
+        '''
+        return f"//{tag}[@{attr}='{val}']"
 
 # soup = BeautifulSoup(response, 'html.parser')
 # print(soup)
+
+if __name__ == '__main__':
+    
+    # https://www.target.com/s?searchTerm=meat&sortBy=relevance&category=5xt1a&Nao=0&moveTo=product-list-grid
+
+    # grocery_list = ['meat', 'produce', 'dairy', 'frozen foods', 'bread AND bakery', 
+    #                 'beverages', 'baking goods', 'pantry food',  'seafood', 'snacks', 
+    #                 'pastry', 'deli', 'coffee']
+
+    grocery_list = ['meat', 'produce']
+
+    wd = webdriver.Chrome()
+
+    # pagination html info
+    tag = 'span'
+    attr = 'class'
+    val = 'Pagination__StyledSpan-sc-sq3l8r-5 gyBTAO'
+
+    pg_num_xpath = TargetScrapper.make_xpath(tag, attr, val)
+    url = 'https://www.target.com/s?searchTerm=meat&sortBy=relevance&category=5xt1a&Nao=0&moveTo=product-list-grid'
+    bot = TargetScrapper(wd, url, pg_num_xpath, grocery_list)
+    bot.find_products()
