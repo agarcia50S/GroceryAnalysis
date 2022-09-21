79cefa3ec3ebd0f6f35ba4ce06f452db62a50859
agarcia50S <agarcia50@luc.edu>
2022-09-21 16:16:53 -0500

    add static mthd for str parsing

--- GroceryPrices/structure_builder.py
+++ GroceryPrices/structure_builder.py
@@ -8,6 +8,14 @@ class Storage():
         soup = BeautifulSoup(self.pages, 'html.parser') 
         html_elmnts = soup.find_all(tag, class_=tag_class)
         return [i.text for i in html_elmnts]
+         
+    @staticmethod
+    def text_parser(text, delim):
+        split_loc = text.find(delim)
+        return text[:split_loc]
+
+    
+
 
 
 
