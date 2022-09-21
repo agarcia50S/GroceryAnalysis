c7eb3642caabdb555e6522d0cfc951185e9cfe52
agarcia50S <agarcia50@luc.edu>
2022-09-21 15:29:19 -0500

    def fnc that returns list of text from html elmnts

--- GroceryPrices/structure_builder.py
+++ GroceryPrices/structure_builder.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
 class Storage():
-    def __init__(self, template):
-        self.container = template
-    def find_html(self, page_html,  tag, tag_class):
-        pass
+    def __init__(self, template, pages):
+        self.template = template
+        self.pages = pages
+    def extract_text(self, page_html, tag, tag_class):
+        soup = BeautifulSoup(page_html, 'html.parser') 
+        html_elmnts = soup.find_all(tag, class_=tag_class)
+        return [i.text for i in html_elmnts]
+
+
