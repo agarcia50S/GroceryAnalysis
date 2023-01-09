d1a27804fcd15235a6f75e0e5e5d3bb6119b0657
agarcia50S <agarcia50@luc.edu>
2023-01-09 17:48:49 -0600

    remove useless modules

--- GroceryPrices/jewelosco_scrapper.py
+++ GroceryPrices/jewelosco_scrapper.py
@@ -1,11 +1,5 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-
 from time import sleep
 from bs4 import BeautifulSoup
-
 import csv
 
 class JewelOscoScrapper:
