40078a90c8a34c2add25d09f6896a1feefe2c2aa
agarcia50S <agarcia50@luc.edu>
2023-03-02 12:24:59 -0600

    remove useless fnc

--- GroceryPrices/jo_scrapper_beta.py
+++ GroceryPrices/jo_scrapper_beta.py
@@ -3,10 +3,7 @@ import json
 # make a url that can locate the results page of a given product
 # full url = base url + search parameter (i.e. item from grocery list)
 
-# https://www.jewelosco.com/abs/pub/xapi/pgmsearch/v1/search/products?request-id=3981677549256748407&url=https://www.jewelosco.com&pageurl=https://www.jewelosco.com&pagename=search&rows=30&start=0&search-type=keyword&storeid=3441&featured=true&search-uid=&q=riceselect rice&sort=&featuredsessionid=&screenwidth=1533&dvid=web-4.1search&channel=instore&banner=jewelosco
-def make_request_url(search_word):
-        prod_data_url = f"https://www.jewelosco.com/abs/pub/xapi/pgmsearch/v1/search/products?request-id=3981677549256748407&url=https://www.jewelosco.com&pageurl=https://www.jewelosco.com&pagename=search&rows=30&start=0&search-type=keyword&storeid=3441&featured=true&search-uid=&q={search_word}&sort=&featuredsessionid=&screenwidth=1533&dvid=web-4.1search&channel=instore&banner=jewelosco"
-        return prod_data_url
+
 
 def format_cookies(cookie_pairs):
      '''
